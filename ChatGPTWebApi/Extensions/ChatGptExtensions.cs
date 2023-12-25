2d5a1e8bd6145e78ca9d276026880c2424c83542
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-25 17:35:57 -0300

    Created ChatGPT Extensions class

---
+++ ChatGPTWebApi/Extensions/ChatGptExtensions.cs
@@ -0,0 +1,15 @@
+﻿using OpenAI_API;
+
+namespace ChatGPTWebApi.Extensions
+{
+    public static class ChatGptExtensions
+    {
+        public static WebApplicationBuilder AddChatGpt(this WebApplicationBuilder builder)
+        {
+            var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+            var chat = new OpenAIAPI(key);
+            builder.Services.AddSingleton(chat);
+            return builder;
+        }
+    }
+}

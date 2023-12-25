c053ade9caebc2e902348e4bf128c2a05e1127c7
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-25 17:36:23 -0300

    Update in program

--- ChatGPTWebApi/Program.cs
+++ ChatGPTWebApi/Program.cs
@@ -1,6 +1,10 @@
+using ChatGPTWebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.AddChatGpt();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

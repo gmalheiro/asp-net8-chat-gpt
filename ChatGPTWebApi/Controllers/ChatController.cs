180c8231a3fb1d76a1277b912c15a2c4f1003226
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-25 19:26:58 -0300

    Update in ChatController

---
+++ ChatGPTWebApi/Controllers/ChatController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+
+namespace ChatGPTWebApi.Controllers
+{
+    [Route("bot/[controller]")]
+
+    public class ChatController : Controller
+    {
+        private readonly OpenAIAPI? _chatGpt ;
+
+        public ChatController(OpenAIAPI? chatGpt)
+        {
+            _chatGpt = chatGpt;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Chat([FromQuery(Name = "prompt")] string prompt)
+        {
+            var response = "";
+
+            var completion = new CompletionRequest
+            {
+                Prompt = prompt,
+                Model = Model.DavinciText,
+                MaxTokens = 200
+            };
+            var result = await _chatGpt?.Completions.CreateCompletionAsync(completion)!;
+            result.Completions.ForEach(resultText => response = resultText.Text);
+
+            return Ok(response);
+        }
+    }
+}

a3514d880e0606ef822c6052f41782e4d1e88647
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-25 17:41:49 -0300

    Created class SerilogExtensions

---
+++ ChatGPTWebApi/Extensions/SerilogExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace ChatGPTWebApi.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder,IConfiguration configuration,string appName)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Host.UseSerilog(Log.Logger, true);
+
+            return builder;
+        }
+    }
+}

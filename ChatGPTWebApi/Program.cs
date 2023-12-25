93e9bf32905f7022e3f3c23bcb295888cab1548d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-25 17:49:12 -0300

    Update in program

--- ChatGPTWebApi/Program.cs
+++ ChatGPTWebApi/Program.cs
@@ -3,11 +3,14 @@ using ChatGPTWebApi.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var appName = "ChatGPT ASP.NET 8 Integration";
 
 builder.AddChatGpt();
 builder.AddSerilog(builder.Configuration,"AspNet8 with ChatGPT");
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwagger(builder.Configuration, appName);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,11 +18,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerDoc(appName);
 
 app.UseHttpsRedirection();
 
